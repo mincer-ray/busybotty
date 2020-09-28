dc439c5cd59edb3b3cd7cfea2e05918e64a847e8
Peter De Genaro <pdegenaro@gmail.com>
2020-09-28 14:06:32 -0400

    switch env vars

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { App } = require('@slack/bolt');
 
-const secrets = require('./secrets');
+let secrets = null;
+if (process.env.BOT_ENV === 'PRODUCTION') {
+  secrets = {
+    SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
+    SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN,  
+  }
+} else {
+  secrets = require('./secrets');
+}
 
 const { createServer } = require('http');
 const express = require('express');
